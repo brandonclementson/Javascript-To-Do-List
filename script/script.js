c9790956984d6f72917382565817054bfd8639da
bclementson <brandon.clementson@gmail.com>
2019-04-17 19:57:33 -0700

    Added icons with hover states

--- script/script.js
+++ script/script.js
@@ -15,31 +15,30 @@ document.querySelector('.add-to-list').addEventListener('keydown', function () {
         var itemValue = itemField.value;
         itemField.value = ''
 
-
         var newToDo = document.createElement('li');
         newToDo.classList.add('list-item');
+        newToDo.classList.add('animated', 'fadeIn');
 
         var buttons = document.createElement('div');
         buttons.classList.add('list-div');
 
-        var completeItem = document.createElement('input');
-        completeItem.type = 'image'
-        completeItem.src = 'img/bx-circle.svg';
-        completeItem.classList.add('complete-item');
+        var completeItem = document.createElement('i');
+        completeItem.classList.add('complete-item', 'far', 'fa-circle');
+        completeItem.addEventListener('mouseenter', iconHover);
+        completeItem.addEventListener('mouseout', iconHover);
         completeItem.addEventListener('click', toggleComplete);
 
 
         var item = document.createElement('p');
         item.textContent = itemValue;
 
-        var removeItem = document.createElement('input');
-        removeItem.type = 'image'
-        removeItem.src = 'img/bxs-trash.svg';
-        removeItem.classList.add('remove-item');
+        var removeItem = document.createElement('i');
+        removeItem.classList.add('remove-item', 'fas', 'fa-trash-alt');
         removeItem.addEventListener('click', removeFromList);
 
+        // Add elements to DOM
         buttons.appendChild(completeItem);
-        buttons.appendChild(item)
+        buttons.appendChild(item);
         buttons.appendChild(removeItem);
         newToDo.appendChild(buttons);
 
@@ -50,19 +49,40 @@ document.querySelector('.add-to-list').addEventListener('keydown', function () {
 
 function removeFromList() {
     var item = this.parentNode.parentNode;
-    var parent = item.parentNode;
-    parent.removeChild(item);
+    var parentList = item.parentNode;
+    parentList.removeChild(item);
 }
 
 function toggleComplete() {
     var item = this.parentNode.parentNode;
-    var parent = item.parentNode;
+    var parentList = item.parentNode;
 
-    parent.removeChild(item);
+    parentList.removeChild(item);
 
-    if (parent.classList.contains('to-do-list')) {
+    // Check if item is in to-do list and move to completed
+    if (parentList.classList.contains('to-do-list')) {
+
+        this.classList.remove('far', 'fa-circle', 'fa-check-circle');
+        this.classList.add('fas', 'fa-check-circle');
+
         completedList.insertBefore(item, completedList.childNodes[0]);
-    } else if (parent.classList.contains('completed-list')) {
+
+    } else if (parentList.classList.contains('completed-list')) {
+
+        this.classList.remove('fas', 'fa-check-circle');
+        this.classList.add('far', 'fa-circle');
+
         list.insertBefore(item, list.childNodes[0]);
+    }
+}
+
+function iconHover() {
+    if (this.classList.contains('fa-circle')) {
+        this.classList.remove('fa-circle');
+        this.classList.add('fa-check-circle');
+    } else if (this.classList.contains('fa-check-circle')) {
+        this.classList.remove('fa-check-circle');
+        this.classList.add('fa-circle');
+
     }
 }
